d4463d2bf334281c6f480a1aec397edd7131d7df
Ben Hayden <benjhayden@chromium.org>
2019-01-11 14:02:07 -0800

    merging

--- tracing/tracing/value/diagnostics/breakdown_unittest.py
+++ tracing/tracing/value/diagnostics/breakdown_unittest.py
@@ -4,7 +4,6 @@
 
 import json
 import math
-import sys
 import unittest
 
 from tracing.value.diagnostics import breakdown
@@ -13,6 +12,12 @@ from tracing.value.diagnostics import diagnostic
 
 class BreakdownUnittest(unittest.TestCase):
 
+  def testZero(self):
+    bd = breakdown.Breakdown()
+    self.assertEqual(0, len(bd))
+    bd.Set('b', 0)
+    self.assertEqual(0, len(bd))
+
   def testRoundtrip(self):
     bd = breakdown.Breakdown()
     bd.Set('one', 1)
@@ -20,7 +25,7 @@ class BreakdownUnittest(unittest.TestCase):
     bd.Set('inf', float('inf'))
     bd.Set('nun', float('nan'))
     bd.Set('ninf', float('-inf'))
-    bd.Set('long', 1 + sys.maxint)
+    bd.Set('long', 2**65)
     d = bd.AsDict()
     clone = diagnostic.Diagnostic.FromDict(d)
     self.assertEqual(json.dumps(d), json.dumps(clone.AsDict()))
@@ -29,4 +34,4 @@ class BreakdownUnittest(unittest.TestCase):
     self.assertEqual(clone.Get('inf'), float('inf'))
     self.assertTrue(math.isnan(clone.Get('nun')))
     self.assertEqual(clone.Get('ninf'), float('-inf'))
-    self.assertEqual(clone.Get('long'), 1 + sys.maxint)
+    self.assertEqual(clone.Get('long'), 2**65)
